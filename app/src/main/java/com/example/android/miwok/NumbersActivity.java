5bb3f667053a29f99613f05f77f0aa97fa9ee2f3
Paul Mburu <paulmburu53@gmail.com>
2018-09-15 23:21:27 +0300

    using ListView and ArrayAdapter for recycling view

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -45,14 +47,10 @@ public class NumbersActivity extends AppCompatActivity {
         for(int i=0;i<words.size();i++)
             Log.v("NumberActivity","word at index "+i+" "+words.get(i));
 
+        ArrayAdapter<String> itemsAdapter =new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,words);
 
-        LinearLayout rootView=(LinearLayout)findViewById(R.id.rootView);
-
-        for(int i=0;i<words.size();i++){
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(i));
-            rootView.addView(wordView);
-        }
+        ListView listView=(ListView)findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
 
     }
